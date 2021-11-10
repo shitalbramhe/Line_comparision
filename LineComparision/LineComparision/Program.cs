535fdbdefcb5a4e2e1de6628c6169ee4a47bbf12
shital bramhe <shitalbramhe@gmail.com>
2021-11-10 22:28:22 +0530

    [shital] Add : initial

---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("... Welcome to Line Comparison Computation ... ");
+            Line_comparision Linecom = new Line_comparision();
+            Linecom.length_line();
+            Console.ReadKey();
+        }
+    }
+}
